d98ff9f74868c7ef957a377795b3fee2d7474094
lexus <lexuszhi1990@gmail.com>
2013-03-20 21:15:33 +0800

    add rspec and gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Riedhunter::Application.config.secret_token = '02f686be3aaadd4fba59783ffcd3c699a4926b964571825b8c63882bb5a49842183a9a9d3ec56ad7f662961a0e300f6fb4a2486efcf473dc338a8a4f892989c1'
