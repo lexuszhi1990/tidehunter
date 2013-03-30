8e349696c64a12ac0ee0e145dd9974f29a23281f
lexus <lexuszhi1990@gmail.com>
2013-03-30 17:46:41 +0800

    login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+  end
+
+  def create
+    #binding.pry
+    user = User.new(name: params[:username], email: params[:email], password: params[:password])
+    if user.save
+      flash[:notice] = "Welcome to Here"
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:notice] = "Wrong user"
+      user.errors.delete(:password_digest)
+      render :new
+    end
+  end
 end
