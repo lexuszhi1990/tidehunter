281192f30cade3470e21c2014a0fa7e2da282208
lexus <lexuszhi1990@gmail.com>
2013-03-31 19:47:11 +0800

    start the homepage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ActiveRecord::Base
-  attr_accessible :body, :title, :url
+  attr_accessible :body, :title, :tag
   
   belongs_to :user
   has_many :comments
 
   validates :body, :presence => true
   validates :title, :presence => true
-  validates :url, :presence => true
+  validates :tag, :presence => true
 
 end
