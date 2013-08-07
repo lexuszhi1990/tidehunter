f25c2c3224b86da92de92c447676d05348733e77
David <david@beansmile.com>
2013-08-07 23:53:56 +0800

    use markdown.js to preview the article

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,7 +1,12 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-# preview = $('#text-preview'))
-# text = $('.lab_article .mainarea').text())
-# mark-text = markdown.toHTML(text))
-# preview.html(mark))
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+$(function(){
+  $preview = $('#preview-contents');
+  $('.lab_article .mainarea').keyup(function(event) {
+    console.log(event.keyCode);
+    markDownText = markdown.toHTML($(this).val());
+    $preview.html(markDownText);
+  });
+});
